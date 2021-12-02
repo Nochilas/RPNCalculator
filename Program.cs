950992a09cd850a061c1d508f0531d557a00cafd
Nochilas <patrononicholas@gmail.com>
2021-12-02 15:29:43 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace proj1
+{
+    class RPN
+    {
+        static double[] reg = new double[4];
+
+        static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        static double Multiply(double x, double y)
+        {
+            return (x * y);
+        }
+
+        static double Divide(double x, double y)
+        {
+            return (x / y);
+        }
+
+        static double Factorial(int x)
+        {
+            if (x == 0)
+                return 1;
+            
+            int res = 1;
+
+            for (int i = 1; i<=x; i++)
+                res *= i;
+            
+            return res;
+        }
+        static void Main()
+        {
+            int x, y;
+            char op;
+            
+            Console.Write("Inserisci primo numero: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Inserisci secondo numero: ");
+            y = int.Parse(Console.ReadLine());
+            Console.Write("Inserisci operatore: ");
+            op = Convert.ToChar(Console.ReadLine());
+
+            switch (op)
+            {
+                case '+':
+                    Console.Write(Sum(x, y));
+                    break;
+                
+                case '-':
+                    Console.Write(Subtract(x, y));
+                    break;
+
+                case '*':
+                    Console.Write(Multiply(x, y));
+                    break;
+
+                case '/':
+                    Console.Write(Divide(x, y));
+                    break;
+
+                default:
+                    Console.Write("Errore");
+                    break;
+            }
+        }
+    }
+}
