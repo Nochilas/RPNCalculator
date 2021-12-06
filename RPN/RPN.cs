4320c4312e4961d74390a06ee8186a9f1d61f276
Nochilas <patrononicholas@gmail.com>
2021-12-06 16:26:32 +0100

    Flush

--- RPN/RPN.cs
+++ RPN/RPN.cs
@@ -11,22 +11,49 @@ namespace calculator
             private set { regs = value; }
         }
 
-        private int regCount;
+        private readonly int regCount;
         public int RegCount
         {
             get { return regCount; }
-            set { regCount = value; }
         }
-      
+
         public RPN (int regNum)
         {
-            if(regCount < 2)
+            if(regNum < 2)
             {
-                throw new ArgumentException("Error");    
+                throw new ArgumentException("New Argument Exception: registers number can't be lower than 2");
             }
 
             regCount = regNum;
             regs = new double[regNum];
+        }
+
+        public RPN ()
+            : this(4) {}
+        
+        
+        public void Sum()
+        {
+            regs[0] = regs[0] + regs[1];
+            regs[1] = 0;
+            regs[regCount - 2] = regs[regCount - 1];
+        }
+
+        private void FlushUp(double n)
+        {
+            for(int i = regCount - 1; i > 0; i--)
+            {
+                regs[i] = regs[i - 1];
+            }
+
+            regs[0] = n;
+        }
+
+        public override string ToString()
+        {
+            return "";
+            //Y: reg[1]
+            //X: reg[0]
         }
     }
 }
