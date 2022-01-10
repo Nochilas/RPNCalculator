272e95def1e5b0702b77dccf98c30b15e6c15259
Nochilas <patrononicholas@gmail.com>
2022-01-10 20:55:20 +0100

    Fix Client & Server dirs

--- RPNserver/Program.cs
+++ RPNserver/Program.cs
@@ -1,25 +1,73 @@
 ﻿using System;
-using RPNlib;
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;
 
-namespace calculator
+namespace RPNclient
 {
     class Program
-    {  
-        static void Main()
+    {
+        static void Main(string[] args)
         {
-            int regCount;
-            string command;
-            Manager manager;
-
-            Console.WriteLine("Inserire numero di registri");
-            regCount = Convert.ToInt32(Console.ReadLine());
-            manager = new Manager(regCount);
-            
-            Console.WriteLine("Inserire comando");
-            command = Console.ReadLine();            
-            manager.ReadCommand(command);
-            
-            Console.WriteLine(manager.RPN.ToString());
+            Console.WriteLine("Hello World!");
         }
+
+        public static void RunServer() {  
+        // Data buffer for incoming data.  
+        byte[] bytes = new Byte[1024];  
+
+        // Establish the local endpoint for the socket.  
+        // Dns.GetHostName returns the name of the
+        // host running the application.  
+        IPHostEntry ipHostInfo = Dns.GetHostEntry("127.0.0.1");  
+        IPAddress ipAddress = ipHostInfo.AddressList[0];  
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        // Create a TCP/IP socket.  
+        Socket listener = new Socket(ipAddress.AddressFamily,  
+            SocketType.Stream, ProtocolType.Tcp );  
+  
+        // Bind the socket to the local endpoint and
+        // listen for incoming connections.  
+        try {  
+            listener.Bind(localEndPoint);  
+            listener.Listen(10);  
+  
+            // Start listening for connections.  
+            while (true) {  
+                Console.WriteLine("Waiting for a connection...");  
+                // Program is suspended while waiting for an incoming connection.  
+                Socket handler = listener.Accept();  
+                string data = null;  
+  
+                // An incoming connection needs to be processed.  
+                while (true) {  
+                    int bytesRec = handler.Receive(bytes);  
+                    data += Encoding.ASCII.GetString(bytes,0,bytesRec);  
+                    if (data.IndexOf("<EOF>") > -1) {  
+                        break;  
+                    }  
+                }  
+  
+                // Show the data on the console.  
+                Console.WriteLine( "Text received : {0}", data);  
+  
+                // Echo the data back to the client.  
+                byte[] msg = Encoding.ASCII.GetBytes(data);  
+  
+                handler.Send(msg);  
+                handler.Shutdown(SocketShutdown.Both);  
+                handler.Close();  
+            }  
+  
+        } catch (Exception e) {  
+            Console.WriteLine(e.ToString());  
+        }  
+  
+        Console.WriteLine("\nPress ENTER to continue...");  
+        Console.Read();  
+  
+    }
+    
     }
 }
